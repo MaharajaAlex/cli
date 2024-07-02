ede8f03fa935993d5bad323ae172a81dc816f159
Ananyo Bhattacharya <ananyobhattacharya.14.15.0012@gmail.com>
2024-07-02 22:02:36 +0530

    exit function and documentation added

--- src/commands.py
+++ src/commands.py
@@ -47,37 +47,91 @@ def cd(path: Union[str, bytes]) -> None:
     except OSError as e:
         print(f"Error: {e}")
 
-def shutdown():
+def shutdown() -> None:
+    """
+    Shutdown the system.
+    
+    Parameters:
+    None 
+
+    Raises:
+    OSError: For other OS-related errors.
+    """
     try:
         os.system("shutdown")
     except OSError as e:
         print(f"Error: {e}")
 
-def signout():
+def signout() -> None:
+    """
+    Signout the user.
+    
+    Parameters:
+    None 
+
+    Raises:
+    OSError: For other OS-related errors.
+    """
     try:
         os.system("shutdown /l")
     except OSError as e:
         print(f"Error: {e}")
 
 def mkdir(path: Union[str, bytes]) -> None:
+    """
+    Make a new directory.
+    
+    Parameters:
+    path (str | bytes): The path of the directory to be made.
+
+    Raises:
+    OSError: For other OS-related errors.
+    """
     try:
         os.mkdir(path, mode=0o777, dir_fd=None)
     except OSError as e:
         print(f"Error: {e}")
 
 def rmdir(path: Union[str, bytes]) -> None:
+    """
+    Remove a directory.
+    
+    Parameters:
+    path (str | bytes): The path of the directory to be removed.
+
+    Raises:
+    OSError: For other OS-related errors.
+    """
     try:
         os.rmdir(path)
     except OSError as e:
         print(f"Error: {e}")
 
-def echo(text):
+def echo(text: Union[str, bytes]):
+    """
+    Display user input text.
+    
+    Parameters:
+    text (str | bytes): The text to be displayed.
+
+    Raises:
+    OSError: For other OS-related errors.
+    """
     try:
         print(text)
     except OSError as e:
         print(f"Error: {e}")
 
 def cls():
+    """
+    Clear the screen.
+    
+    Parameters:
+    None
+
+    Raises:
+    OSError: For other OS-related errors.
+    """
     try:
         print("Cleared")
     except OSError as e:
